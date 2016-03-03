a0897c381307192cb45bee966c4bf1ea8f7dd71b
gelther <lpw.sormano@gmail.com>
2016-03-03 10:02:31 +0100

    Code consistency changes for index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-if ( true )  {
-  echo 'true';
+if ( true ) {
+	echo 'true';
 }
 
-$array[ 1 ] = '1';
+$array[1] = '1';
 $array[2] = '2';
 
 			$values['field'] 		= 'select';
@@ -15,6 +15,6 @@ $array[2] = '2';
 
 
 			$values['field'] 		= 'select';
-			$roles 				= array_keys( get_editable_roles() );
+			$roles = array_keys( get_editable_roles() );
 			$values['options'] 		= array_combine( $roles, $roles );
 			$values['options']		= array_merge( $values['options'], array( 'not_logged_in' => 'Not logged in' ) );
