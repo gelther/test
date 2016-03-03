cd3d3f841eaea3c43788f0c838d3ad03ef164bf7
gelther <lpw.sormano@gmail.com>
2016-03-03 09:59:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,3 +6,8 @@ if ( true )  {
 
 $array[ 1 ] = '1';
 $array[2] = '2';
+
+			$values['field'] 		= 'select';
+			$roles = array_keys( get_editable_roles() );
+			$values['options'] 		= array_combine( $roles, $roles );
+			$values['options']		= array_merge( $values['options'], array( 'not_logged_in' => 'Not logged in' ) );
