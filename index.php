6ad97661c713788065a356f1d1cc1069598651bf
gelther <lpw.sormano@gmail.com>
2016-03-03 10:02:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,3 +11,10 @@ $array[2] = '2';
 			$roles = array_keys( get_editable_roles() );
 			$values['options'] 		= array_combine( $roles, $roles );
 			$values['options']		= array_merge( $values['options'], array( 'not_logged_in' => 'Not logged in' ) );
+
+
+
+			$values['field'] 		= 'select';
+			$roles 				= array_keys( get_editable_roles() );
+			$values['options'] 		= array_combine( $roles, $roles );
+			$values['options']		= array_merge( $values['options'], array( 'not_logged_in' => 'Not logged in' ) );
