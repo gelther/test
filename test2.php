e5b72dda9620942964c36c69812edc1e09086eb1
gelther <lpw.sormano@gmail.com>
2016-03-25 13:30:10 +0100

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,13 +1,3 @@
-
-<?php
-class EDD_Batch_Customers_Export extends EDD_Batch_Export {
-
-	/**
-	 * Set the properties specific to the Customers export
-	 *
-	 * @since 2.4.2
-	 * @param array $request The Form Data passed into the batch processing
-	 */
 	public function set_properties( $request ) {
 		$this->start = isset( $request['start'] )            ? sanitize_text_field( $request['start'] ) : '';
 		$this->end   = isset( $request['end'] )             ? sanitize_text_field( $request['end'] )  : '';
@@ -26,4 +16,3 @@ class EDD_Batch_Customers_Export extends EDD_Batch_Export {
 		$thisdownload = isset( $request['download'] ) ? absint( $request['download'] )   : null;
 		$thisprice_id = empty( $request['edd_price_option'] ) && 0 !== $request['edd_price_option'] ? absint( $request['edd_price_option'] )   : null;
 	}
-}
