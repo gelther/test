7ca4929490399a1bf2443ce4bf70819b2e50d6bd
gelther <lpw.sormano@gmail.com>
2016-03-25 15:08:27 +0100

    Code consistency changes for test2.php

--- test2.php
+++ test2.php
@@ -1,19 +1,29 @@
 
 	public function set_properties( $request ) {
+
 		$this->start    = isset( $request['start'] )            ? sanitize_text_field( $request['start'] ) : '';
 		$this->end      = isset( $request['end'] )             ? sanitize_text_field( $request['end'] )  : '';
 		$this->dwnload  = isset( $request['download'] ) ? absint( $request['download'] )   : null;
 		$this->price_id = empty( $request['edd_price_option'] ) && 0 !== $request['edd_price_option'] ? absint( $request['edd_price_option'] )   : null;
+
 	}
+
+
 	public function set_properties( $request ) {
+
 		$this->start    = true;
 		$this->end      = true;
 		$this->download = true;
 		$this->price_id = true;
+
 	}
+
+
 	public function set_properties( $request ) {
+
 		$thisstart    = true;
 		$thisend      = true;
 		$thisdownload = true;
 		$thisprice_id = true;
+
 	}
