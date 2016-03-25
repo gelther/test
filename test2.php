6635a2f9f2ae2c92ab61c32b9f4ec42ba41f8e97
gelther <lpw.sormano@gmail.com>
2016-03-25 13:29:01 +0100

    Update test2.php

--- test2.php
+++ test2.php
@@ -14,4 +14,16 @@ class EDD_Batch_Customers_Export extends EDD_Batch_Export {
 		$this->download = isset( $request['download'] ) ? absint( $request['download'] )   : null;
 		$this->price_id = empty( $request['edd_price_option'] ) && 0 !== $request['edd_price_option'] ? absint( $request['edd_price_option'] )   : null;
 	}
+	public function set_properties( $request ) {
+		$this->start = isset( $request['start'] )            ? sanitize_text_field( $request['start'] ) : '';
+		$this->end = isset( $request['end'] )             ? sanitize_text_field( $request['end'] )  : '';
+		$this->download = isset( $request['download'] ) ? absint( $request['download'] )   : null;
+		$this->price_id = empty( $request['edd_price_option'] ) && 0 !== $request['edd_price_option'] ? absint( $request['edd_price_option'] )   : null;
+	}
+	public function set_properties( $request ) {
+		$thisstart = isset( $request['start'] )            ? sanitize_text_field( $request['start'] ) : '';
+		$thisend   = isset( $request['end'] )             ? sanitize_text_field( $request['end'] )  : '';
+		$thisdownload = isset( $request['download'] ) ? absint( $request['download'] )   : null;
+		$thisprice_id = empty( $request['edd_price_option'] ) && 0 !== $request['edd_price_option'] ? absint( $request['edd_price_option'] )   : null;
+	}
 }
