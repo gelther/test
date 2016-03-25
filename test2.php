2e319130c6febcb38a4aa78eddb312acea9a0ecb
gelther <lpw.sormano@gmail.com>
2016-03-25 14:24:44 +0100

    Update test2.php

--- test2.php
+++ test2.php
@@ -2,13 +2,13 @@
 	public function set_properties( $request ) {
 		$this->start = isset( $request['start'] )            ? sanitize_text_field( $request['start'] ) : '';
 		$this->end   = isset( $request['end'] )             ? sanitize_text_field( $request['end'] )  : '';
-		$this->download = isset( $request['download'] ) ? absint( $request['download'] )   : null;
+		$this->dwnload = isset( $request['download'] ) ? absint( $request['download'] )   : null;
 		$this->price_id = empty( $request['edd_price_option'] ) && 0 !== $request['edd_price_option'] ? absint( $request['edd_price_option'] )   : null;
 	}
 	public function set_properties( $request ) {
 		$this->start = true;
 		$this->end   = true;
-		$this->download = true;
+		$this->dwnload = true;
 		$this->price_id = true;
 	}
 	public function set_properties( $request ) {
