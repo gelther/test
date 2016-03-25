f6d5523b174d3cf6df8a255c701e945d97f311ef
gelther <lpw.sormano@gmail.com>
2016-03-25 13:30:19 +0100

    Code consistency changes for test2.php

--- test2.php
+++ test2.php
@@ -1,18 +1,28 @@
 	public function set_properties( $request ) {
+
 		$this->start = isset( $request['start'] )            ? sanitize_text_field( $request['start'] ) : '';
 		$this->end   = isset( $request['end'] )             ? sanitize_text_field( $request['end'] )  : '';
 		$this->download = isset( $request['download'] ) ? absint( $request['download'] )   : null;
 		$this->price_id = empty( $request['edd_price_option'] ) && 0 !== $request['edd_price_option'] ? absint( $request['edd_price_option'] )   : null;
+
 	}
+
+
 	public function set_properties( $request ) {
+
 		$this->start = isset( $request['start'] )            ? sanitize_text_field( $request['start'] ) : '';
-		$this->end = isset( $request['end'] )             ? sanitize_text_field( $request['end'] )  : '';
+		$this->end   = isset( $request['end'] )             ? sanitize_text_field( $request['end'] )  : '';
 		$this->download = isset( $request['download'] ) ? absint( $request['download'] )   : null;
 		$this->price_id = empty( $request['edd_price_option'] ) && 0 !== $request['edd_price_option'] ? absint( $request['edd_price_option'] )   : null;
+
 	}
+
+
 	public function set_properties( $request ) {
-		$thisstart = isset( $request['start'] )            ? sanitize_text_field( $request['start'] ) : '';
-		$thisend   = isset( $request['end'] )             ? sanitize_text_field( $request['end'] )  : '';
+
+		$thisstart    = isset( $request['start'] )            ? sanitize_text_field( $request['start'] ) : '';
+		$thisend      = isset( $request['end'] )             ? sanitize_text_field( $request['end'] )  : '';
 		$thisdownload = isset( $request['download'] ) ? absint( $request['download'] )   : null;
 		$thisprice_id = empty( $request['edd_price_option'] ) && 0 !== $request['edd_price_option'] ? absint( $request['edd_price_option'] )   : null;
+
 	}
